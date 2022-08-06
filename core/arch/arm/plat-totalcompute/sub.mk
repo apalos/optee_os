7c89d2fe39806f363f374a9e10bd850e386679f9
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-06 19:41:14 +0800

    Delete core directory

--- core/arch/arm/plat-totalcompute/sub.mk
+++
@@ -1,5 +0,0 @@
-global-incdirs-y += .
-srcs-y += main.c
-ifeq ($(CFG_CORE_FFA),y)
-srcs-y += tc_spmc_pm.c
-endif
--- core/drivers/clk/sub.mk
+++
@@ -1,5 +0,0 @@
-srcs-y += clk.c
-srcs-$(CFG_DRIVERS_CLK_DT) += clk_dt.c
-srcs-$(CFG_DRIVERS_CLK_FIXED) += fixed_clk.c
-
-subdirs-$(CFG_DRIVERS_SAM_CLK) += sam
